de26a5b666edc09c4cb600ca8da5bc138c487a6b
Daniel Gonzalez <danny071x@gmail.com>
2021-06-25 18:47:01 -0500

    prototype layout, minimal functionality

--- GonzalezDaniel-HW4/ViewController.swift
+++ GonzalezDaniel-HW4/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+var supportedOperations = ["Add", "Subtract", "Multiply", "Divide"]
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return supportedOperations.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "protoCell", for: indexPath as IndexPath)
+        let row = indexPath.row
+        cell.textLabel?.text = supportedOperations[row]
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
 }
 
---
+++ GonzalezDaniel-HW4/viewWillAppear.swift
@@ -0,0 +1,17 @@
+//
+//  viewWillAppear.swift
+//  GonzalezDaniel-HW4
+//
+//  Created by Daniel Gonzalez on 6/25/21.
+//
+
+import UIKit
+
+class viewWillAppear: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
